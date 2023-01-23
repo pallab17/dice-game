ba45a0be5abbdf2e83639d08821ebdec5ffdad3c
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-01-23 17:58:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// alert("hi");
+// connection check korchilam
+
+
+var randomNumber1=   Math.floor(Math.random()*6) ;  // 0-5
+randomNumber1=randomNumber1+1;    //1-6
+
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png to dice6.png
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+ image1.setAttribute("src",randomImageSource);
+
+
+ 
+ 
+ var randomNumber2=   Math.floor(Math.random()*6) ;  // 0-5
+ randomNumber2=randomNumber2+1;    //1-6
+ 
+ 
+ var randomDiceImage2 = "dice" + randomNumber2 + ".png"; // dice1.png to dice6.png
+ 
+ var randomImageSource2 = "images/" + randomDiceImage2;
+ 
+ var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src",randomImageSource2);
+ 
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🏁Player 1 wins!!!";
+
+} else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").innerHTML="🏁Player 2 wins!!!";
+
+} else{
+    document.querySelector("h1").innerHTML="Draw....";
+}
